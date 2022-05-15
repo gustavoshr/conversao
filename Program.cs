8c6caf3232eabbc029e4b09cdd2e2fc5178371fa
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:19:54 -0300

    conversao

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // convertendo dados
+            double meudouble = 1.73;
+            int minhaidade = (int) meudouble;
+
+            Console.WriteLine(meudouble);
+            Console.WriteLine(minhaidade);
+
+        }
+    }
+}
